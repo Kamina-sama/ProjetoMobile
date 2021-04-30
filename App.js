79ec66a75b6b83d24ae1f53f925da6c4bdeacefe
jgsneves <jgsn.adv@gmail.com>
2021-04-30 15:31:14 -0300

    last commit

--- App.js
+++ App.js
@@ -13,13 +13,15 @@ import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
 import DeleteAsync from './Screens/DeleteAsync';
 import CommentsScreen from './src/pages/commentsScreen/CommentsScreen'
+import PaymentCards from './src/pages/paymentCards';
+import { DetailedCard } from './src/pages/paymentCards/detailedCard';
 
 const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName={'SignUp'}>
+      <Drawer.Navigator initialRouteName={'PaymentCardList'}>
         <Drawer.Screen name={'SignUp'} component={SignUp} />
         <Drawer.Screen name={'Login'} component={Login} options={{gestureEnabled:false}}/>
         <Drawer.Screen name={'UploadBook'} component={UploadBook}/>
@@ -28,6 +30,8 @@ export default function App() {
         <Drawer.Screen name={'Store'} component={Store}/>
         <Drawer.Screen name={'DeleteAsync'} component={DeleteAsync}/>
         <Drawer.Screen name={'CommentsScreen'} component={CommentsScreen}/>
+        <Drawer.Screen name={'PaymentCardList'} component={PaymentCards} />
+        <Drawer.Screen name={'DetailedCard'} component={DetailedCard} />
       </Drawer.Navigator>
     </NavigationContainer>
   );
